b317293a3e7b2730e7b52543d523e797a0777fcb
Ahmad Haziq Mu'izzaddin Wafiq <107825639+ahmdhzq@users.noreply.github.com>
2022-10-28 14:13:52 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function main() {
+    let nomor = document.getElementById("nomor").value
+    
+    nomor == 2 ? alert("Benar, Angka yang anda masukan adalah 2") : alert(`Salah, Angka yang anda masukan adalah ${nomor}`)
+};
