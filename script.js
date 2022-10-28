605c83ffac4f77c688879dfe5c9530c0beaf42de
Ahmad Haziq Mu'izzaddin Wafiq <107825639+ahmdhzq@users.noreply.github.com>
2022-10-28 15:34:18 +0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function main() {
-    let nomor = document.getElementById("nomor").value
-    
-    nomor == 2 ? alert("Benar, Angka yang anda masukan adalah 2") : alert(`Salah, Angka yang anda masukan adalah ${nomor}`)
-};
